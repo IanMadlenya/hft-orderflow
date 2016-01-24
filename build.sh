d21fb4f87f429f40c2a3048ae2a7223acf7b5258
Johnny Qiu <cyau1982@gmail.com>
2016-01-24 20:43:53 +0800

    Add Huobi broker interface

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 rm -rf build
 mkdir build
-g++ src/main.cpp src/api/restapi.cpp src/api/okcoin.cpp src/api/md5/md5.cpp libjson.a -std=c++11 -lcurl -lpthread -Wall -Werror -O3 -o build/hft-orderflow
+g++ src/main.cpp src/api/restapi.cpp src/api/okcoin.cpp src/api/huobi.cpp src/api/md5/md5.cpp libjson.a -std=c++11 -lcurl -lpthread -Wall -Werror -O3 -o build/hft-orderflow
