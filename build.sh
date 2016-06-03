6139d91e33ff88dd46d35f5b6d44f5d3364aec65
Johnny Qiu <cyau1982@gmail.com>
2016-06-03 09:09:58 +0800

    Update to v1.1
    1. Add FIX interface
    2. Update config

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 rm -rf build
 mkdir build
-g++ src/main.cpp src/api/restapi.cpp src/api/okcoin.cpp src/api/huobi.cpp src/api/md5/md5.cpp libjson.a -std=c++11 -lcurl -lpthread -Wall -Werror -O3 -o build/hft-orderflow
+g++ src/main.cpp src/restapi/restapi.cpp src/restapi/okcoin.cpp src/restapi/huobi.cpp src/md5/md5.cpp src/fixapi/okcoin.cpp src/fixapi/huobi.cpp libjson.a -std=c++11 -lcurl -lpthread -lquickfix -Wall -Werror -Wl,-rpath /usr/local/lib -O3 -o build/hft-orderflow
